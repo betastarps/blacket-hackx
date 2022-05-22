c7c8e4a47576eac481df522ad536ea8de1096168
zastix <69748767+notzastix@users.noreply.github.com>
2022-05-22 15:59:55 -0500

    no dupe elements, made amount of each element based off of chance

--- global/spoofElements.js
+++ global/spoofElements.js
@@ -1,11 +1,9 @@
-for (i=0;i<document.getElementById('#elementList').children.length;i++) {
-    document.getElementById('#elementList').children[i].remove();
-}
+Array.from(document.getElementById('#elementList').children).forEach(a => a.remove())
 Object.entries(elementList).forEach((entry) => {
 	const [key, value] = entry;
 	$(`<img id="${key}" src="${elementList[key].imageURL}" onclick="viewElement('${key}')" class="bottomElement">`).appendTo(".elementList");
 })
 for (i=0;i<Object.keys(elementList).length;i++) {
     elemes = Object.keys(elementList)
-    userElements[elemes[i]] = Math.floor(Math.random() * 100);
+    userElements[elemes[i]] = Math.floor(elementList[elemes[i]]['chance'] / 2 + Math.round(Math.random() * 20));
 }
