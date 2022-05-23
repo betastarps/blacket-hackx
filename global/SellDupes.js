15f125be88fb32e8513d4ddcefafa10c1019b4ac
zastix <69748767+notzastix@users.noreply.github.com>
2022-05-22 22:21:29 -0500

    support for diffrent pages

--- global/SellDupes.js
+++ global/SellDupes.js
@@ -1,5 +1,11 @@
+$.get('/api/user/elements', function(data) {
+    userElements = JSON.parse(data)
+    Object.keys(elementList).forEach(element => {
+        sell(element)
+    })
+})
 async function sell(element) {
-    var amt = userElements[element] - 1
+    var amt = userElements[element] - 0
     if (0 >= amt) return;
     var postData = `element=${element}&quantity=${amt}`;
     $.post(`/api/sell/`, postData, function() {
@@ -7,7 +13,3 @@ async function sell(element) {
         else console.log(`Sold ${amt} ${element}(s)`);
     })
 }
-
-Object.keys(elementList).forEach(element => {
-    sell(element)
-})
