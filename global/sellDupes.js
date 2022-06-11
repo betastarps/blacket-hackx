8cb346aa04643cf78061c4b93ea5b6b5fe27ffc6
zastix <69748767+notzastix@users.noreply.github.com>
2022-06-11 05:15:41 -0500

    minification

--- global/sellDupes.js
+++ global/sellDupes.js
@@ -1,13 +1,11 @@
 $.get('/api/user/elements', function(data) {
-    userElements = JSON.parse(data)
+    window.userElements = JSON.parse(data)
     Object.keys(elementList).forEach(element => sell(element))
 })
 async function sell(element) {
-    var amt = userElements[element] - 1
-    if (0 >= amt) return;
-    var postData = `element=${element}&quantity=${amt}`;
-    $.post(`/api/sell/`, postData, function() {
-        if (isNaN(amt)) return;
-        else console.log(`Sold ${amt} ${element}(s)`);
+    var amt =  window.userElements[element] - 1
+    if (0 >= amt) return
+    $.post(`/api/sell/`, `element=${element}&quantity=${amt}`, function() {
+        isNaN(amt) ? return : console.log(`Sold ${amt} ${element}(s)`)
     })
 }
