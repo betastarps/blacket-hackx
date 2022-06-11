f75b47b69e0d104390b766a9947ad80cd27442ba
zastix <69748767+notzastix@users.noreply.github.com>
2022-06-11 05:16:20 -0500

    Update AppearOffline.js

--- global/AppearOffline.js
+++ global/AppearOffline.js
@@ -1,4 +1,4 @@
-alert('WARNING: you will not be able to trade with this script enabled')
+alert('WARNING: you will not be able to trade/chat with this script enabled')
 socket.disconnect()
 if (location.pathname === '/stats/' || location.pathname === '/stats') {
   document.getElementById('#userElement').style.filter = 'drop-shadow(0px 0px 100px red)'
