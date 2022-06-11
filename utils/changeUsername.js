5985e245f649287eecfc4868eedfda5e3a65d9b2
zastix <69748767+notzastix@users.noreply.github.com>
2022-06-11 05:12:20 -0500

    removed unneeded variable

--- utils/changeUsername.js
+++ utils/changeUsername.js
@@ -5,8 +5,7 @@ function changeUsername() {
     var newUsername = prompt("Enter your new username.");
     var pass = btoa(prompt("Enter your current password"));
 
-    var postData = `username=${newUsername}&password=${pass}`;
-    $.post('/api/changeusername/', postData, function(data) {
+    $.post('/api/changeusername/', `username=${newUsername}&password=${pass}`, function(data) {
         navigator.clipboard.writeText(newUsername);
         alert("copied username to clipboard")
     });
