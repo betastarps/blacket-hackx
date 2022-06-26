bec82080754ab09417f19d43982136cd91b8ff71
zastix <69748767+notzastix@users.noreply.github.com>
2022-06-26 05:32:16 -0500

    Update sellDupes.js

--- global/sellDupes.js
+++ global/sellDupes.js
@@ -2,10 +2,11 @@ $.get('/api/user/elements', function(data) {
     window.userElements = JSON.parse(data)
     Object.keys(elementList).forEach(element => sell(element))
 })
+
 async function sell(element) {
     var amt =  window.userElements[element] - 1
     if (0 >= amt) return
     $.post(`/api/sell/`, `element=${element}&quantity=${amt}`, function() {
-        isNaN(amt) ? return : console.log(`Sold ${amt} ${element}(s)`)
+        isNaN(amt) ? "" : console.log(`Sold ${amt} ${element}(s)`)
     })
 }
