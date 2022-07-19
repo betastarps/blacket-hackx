7435bf706199a1eba6f43b171ad3964865e0417f
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-07-19 00:21:18 -0400

    Betastar Hacks
    Updated script for 2 word support!

--- utils/calculateChances.js
+++ utils/calculateChances.js
@@ -1,16 +1,39 @@
 alert('Script coded by zastix.\nFor more scripts, visit BlacketWare on Github.');
 window.open('https://github.com/BlacketWare/blacket-hacks');
 
-try {
-    let element = prompt('What element are you trying to get?')
-    let amt = prompt('How many crates are you opening?')
-    let amount = prompt('How many are you aiming for?')
-    let chance = elementList[element]['chance']
-    alert('Press OK to calculate')
+var wordCount = confirm('Is the element one word?\nOk = yes, Cancel = no');
+if (wordCount) {
+    try {
+        let element = prompt('What element are you trying to get?').toLowerCase();
+        let amt = prompt('How many crates are you opening?')
+        let amount = prompt('How many are you aiming for?')
+        let chance = elementList[element]['chance']
+        alert('Press OK to calculate')
 
-    let calculation = chance * amt
-    alert('You have a ' + calculation + '% chance to get at least one ' + element + '. (Press OK to continue)')
-    alert('You have a ' + (calculation / amount) + '% chance to get ' + amount + ' ' + element + '.')
-} catch (e) {
-    alert('An error has occurred')
+        let calculation = chance * amt
+        alert('You have a ' + calculation + '% chance to get at least one ' + element + '. (Press OK to continue)')
+        alert('You have a ' + (calculation / amount) + '% chance to get ' + amount + ' ' + element + '.')
+    } catch (e) {
+        alert('An error has occurred, you may not have entered the element name properly.')
+    }
+} else {
+    let twoword = prompt('Enter the element name:').toLowerCase();
+    const splitter = twoword.split(" ");
+    let twowordfin1st = splitter[0];
+    let twowordfin2nd = splitter[1];
+    const splitfin = twowordfin2nd.charAt(0).toUpperCase() + twowordfin2nd.slice(1);
+    
+    try {
+        let element = twowordfin1st + splitfin;
+        let amt = prompt('How many crates are you opening?')
+        let amount = prompt('How many are you aiming for?')
+        let chance = elementList[element]['chance']
+        alert('Press OK to calculate')
+
+        let calculation = chance * amt
+        alert('You have a ' + calculation + '% chance to get at least one ' + twoword + '. (Press OK to continue)')
+        alert('You have a ' + (calculation / amount) + '% chance to get ' + amount + ' ' + twoword + '.')
+    } catch (e) {
+        alert('An error has occurred, you may not have entered the element name properly.')
+    }
 }
