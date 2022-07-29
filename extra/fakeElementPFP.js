5a4ad6082c920bd442e03c8a473b917059ecdff2
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-07-29 00:24:36 -0400

    Create fakeElementPFP.js

---
+++ extra/fakeElementPFP.js
@@ -0,0 +1,56 @@
+if (userData.perm > 2) {
+  alert('Yay, you have the permissions level to run this!');
+  async function elementToFile(e) {
+    var out = await $.getJSON(
+      "https://betastar.org/api/elements"
+    );
+
+    var imageurl = "https://betastar.org/" + out[e].imageURL;
+
+    var blob = await fetch(imageurl).then(res => res.blob());
+    return new File(
+      [blob],
+      "custom.png", {
+        type: "image/png"
+      }
+    )
+  }
+
+  function customAvatar(image) {
+    var imageToUpload = new FormData();
+    imageToUpload.append('file', image);
+    $.ajax({
+      url: 'https://betastar.org/api/customavatar/',
+      data: imageToUpload,
+      processData: false,
+      contentType: false,
+      type: 'POST',
+      success: function(data) {
+        if (data === 'SUCCESS') {
+          alert("element changed succesfully. please reactivate the tool.");
+          location.reload()
+        }
+      }
+    })
+  }
+  document.addEventListener('keydown', (e) => {
+    if (e.key.toLowerCase() === 'e' && e.ctrlKey) {
+      e.preventDefault();
+      var element = prompt("element or 'cancel'");
+
+      if (!Object.keys(elementList).includes(element) && element !== "cancel") {
+        alert("that is not a real element. please try again.")
+      }
+      if (element === "cancel") {
+        return
+      }
+
+      elementToFile(element).then(f => {
+        console.log("retrieved file")
+        customAvatar(f)
+      })
+    }
+  });
+} else {
+  alert('Oops! This is for those with a role you don\'t have. Buy Epic in the Store to unlock this feature!\n\nRequired Perm Level: 3+\nCurrent Perm Level: ' + userData.perm);
+}
